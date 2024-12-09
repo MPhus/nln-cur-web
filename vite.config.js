0e8b82bef9dedfadaaeb333acf48d02e3fdec57f
MPhus <phu3009@gmail.com>
2024-12-09 11:47:29 +0700

    config vite 02

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   define: {
-    'process.env': {}
+    'process.env': process.env
   },
   plugins: [
     react(),
