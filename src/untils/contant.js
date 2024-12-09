1e7b2a27169273d88ecebd5ed16ce14afc17a0b9
MPhus <phu3009@gmail.com>
2024-12-09 11:51:43 +0700

    Log env

--- src/untils/contant.js
+++ src/untils/contant.js
@@ -3,12 +3,14 @@
 
 export const API_CITY = 'https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json'
 let apiRoot = 'https://nln-cur-api.onrender.com'
-if (process?.env.BUILD_MODE === 'dev') {
+if (process.env.BUILD_MODE === 'dev') {
 	apiRoot = 'http://localhost:3000'
 }
-if (process?.env.BUILD_MODE === 'production') {
+if (process.env.BUILD_MODE === 'production') {
 	apiRoot = 'https://nln-cur-api.onrender.com'
 }
+console.log('process.env: ', process.env)
+console.log('import.meta.env: ', import.meta.env)
 
 
 export const API_ROOT = apiRoot
